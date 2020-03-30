9401c4a624048f2c473309bd02ebc8d7129fcfa3
Eduardopinatec <a01027604@itesm.mx>
2020-03-30 15:14:23 -0600

    Update March 30, 2020

--- EjerciciosCpp/ActividadExtra/ActExtra.cpp
+++ EjerciciosCpp/ActividadExtra/ActExtra.cpp
@@ -2,26 +2,120 @@
 
 using namespace std;
 
-int fibonacci (int index, int num, int antes){
-    index--;
-    int tot = 0;
-    if (index == 0){
-        return num;
-    }
-    else{
-        if (antes == 0){
-            return fibonacci(index, num, num);
+class Fibo {
+    private:
+    int fibonacciAux (int index, int num, int antes){
+        index--;
+        int tot = 0;
+        if (index == 0){
+            return num;
         }
         else{
-            tot = num+antes;
-            return fibonacci(index, tot, num);
+            if (antes == 0){
+                return fibonacciAux(index, num, num);
+            }
+            else{
+                tot = num+antes;
+                return fibonacciAux(index, tot, num);
+            }
         }
     }
-}
+    public:
+        int fibonacci (int index){
+        return fibonacciAux(index, 1, 0);
+        }
+};
+class Secuencial{
+    public:
+        int busquedaSecuencial(int* a, int buscado, int size){
+            return busquedaSecuencialAux(a, buscado, size, 0);
+        }
+    private:
+        int busquedaSecuencialAux(int* a, int buscado, int size, int pos){
+            if (a[pos]==buscado){
+                return pos;
+            }
+            else if (pos>size){
+                return -1;
+            }
+            else
+            {
+                return busquedaSecuencialAux(a, buscado, size, pos+1);
+            }
+        }
 
+};
+class Ordenada1{
+    public:
+        int busquedaOrdenada1(int *a, int buscado, int size){
+            return busquedaOrdenada1Aux (a, buscado, size, 0);
+        }
+    private:
+        int busquedaOrdenada1Aux(int* a, int buscado, int size, int pos){
+            if (buscado>a[size-1]||pos>size){
+                return -1;
+            }
+            else if (a[pos]==buscado){
+                return pos;
+            }
+            else{
+                return busquedaOrdenada1Aux(a, buscado, size, pos+1);
+            }            
+        }
+};
+class Ordenada2{
+    public:
+        int busquedaOrdenada2(int *a, int size, int buscado, int paso){
+            return busquedaOrdenada2Aux(a, size, buscado, paso, 0);
+        }
+        int busquedaAux(int *a, int inicio, int fin, int buscado){
+            if (a[inicio]==buscado){
+                return inicio;
+            }
+            else if (inicio>fin){
+                return -1;
+            }
+            else{
+                return busquedaAux(a, inicio+1, fin, buscado);
+            }
+        }
+    private:
+        int busquedaOrdenada2Aux(int* a, int size, int buscado, int paso, int pos){
+            int fin = pos+paso-1;
+            if(size-1<fin){
+                fin = size-1;
+            }
+            if(a[fin]>=buscado){
+                return busquedaAux(a, pos, fin, buscado);
+            }
+            else if(pos>size){
+                return -1;
+            }
+            else{
+                return busquedaOrdenada2Aux(a, size, buscado, paso, pos+paso);
+            }
+        }
+};
 int main (){
-    int index = 0;
-    cout<<"Give me an index"<<endl;
-    cin>>index;
-    cout<<fibonacci(7,1,0)<<endl;
+    Fibo Fibo;
+    int fib = 0;
+    cout<<"Que posición fibonacci quieres?"<<endl;
+    cin>>fib;
+    cout<<Fibo.fibonacci(fib)<<endl;
+    Secuencial Secu;
+    int num = -1;
+    int size=5;
+	int *a=new int[size]();
+    a[0]=1;
+    a[1]=3;
+    a[2]=5;
+    a[3]=7;
+    a[4]=9;
+    cout<<"Que numero deseas buscar?"<<endl;
+    cin>>num;
+    cout<<Secu.busquedaSecuencial(a,num,size)<<endl;
+    Ordenada1 Ordenadito1;
+    cout<<Ordenadito1.busquedaOrdenada1(a, num, size)<<endl;
+    Ordenada2 Ordenadito2;
+    cout<<Ordenadito2.busquedaOrdenada2(a, size, num, 2)<<endl;
 }
---
+++ EjerciciosCpp/sort.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class Sort{
+	public:
+	virtual void sort(T *a, int size)=0;
+
+	void intercambiar(T *a, int p1, int p2){
+        T x= a[p1];
+        a[p1] = a[p2];
+        a[p2] = x;
+    }
+
+	void imprimirArreglo(T *a, int size){
+		cout<<a[0];
+		for(int i=1; i<size; i++){
+			cout<<", "<<a[i];
+		}
+		cout<<endl;
+	}
+};
+
+template <class T>
+class LuckySort: public Sort<T>{
+	public:
+	void sort(T *a, int size){
+		this->intercambiar(a, 0, size-1);
+	}
+};
+
+template <class T>
+class SelectionSort: public Sort<T>{
+	public:
+	void sort(T *a, int size){
+		for(int i=0; i<size-1; i++){
+			int p=i;
+			for(int j=i+1; j<size; j++){
+				if(a[j]<a[p]){
+					p=j;
+				}
+			}
+			this->intercambiar(a, i, p);
+		}
+	}
+};
+
+template <class T>
+class BubbleSort: public Sort<T>{
+	public:
+    void sort (T *a, int size){
+		int it = 0;
+        for (int i = 0; i< size; i++) {
+            for (int j = 0; j<size-1-i; j++){
+				if (a[j]>a[j+1]){
+					this->intercambiar
+				}
+			}
+        }
+		cout<<"Vueltas: "<<it<<endl;
+    }
+};
+
+template <class T>
+class BubbleSort: public Sort<T>{
+	public:
+    void sort (T *a, int size){
+        for (int i = 0; i< size; i++) {
+            for (int j = 0; j<size-1-i; j++){
+				if (a[j]>a[j+1]){
+					this->intercambiar
+				}
+			}
+        }
+		cout<<"Vueltas: "<<it<<endl;
+    }
+};
+
+int main(){
+	int size=5;
+	int a[size]={5,4,3,2,1};
+	SelectionSort<int> s;
+	s.imprimirArreglo(a, size);
+	s.sort(a, size);
+	s.imprimirArreglo(a,size);
+	return 0;
+}
